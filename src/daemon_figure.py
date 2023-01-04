2efb62e8b01279b047d640db86a27f1d4dd83d65
RezaEs79 <ereza1397@gmail.com>
2023-01-04 16:13:07 +0330

    finish matplotlib part

--- src/daemon_figure.py
+++ src/daemon_figure.py
@@ -1,73 +1,66 @@
-import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import os
 
-dpi=72
-fig=plt.figure(1,figsize=(1500/dpi,800/dpi),dpi=dpi,frameon=False)
-ax1=fig.add_axes([0,0,1,
-1])
-fig.set_facecolor('xkcd:salmon')
-fig.set_facecolor((1.0, 0.47, 0.42))
-img = mpimg.imread(f"{os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'resources', ))}/image.jpeg")
-imgplot=ax1.imshow(img,aspect=.9)
+# Set Background Color and dpi and First Layer
+plt.rcParams["axes.facecolor"] = "red"
 
-x=np.linspace(0,np.pi*10,300)
+dpi = 100
+fig = plt.figure(1, figsize=(3024/dpi, 1632/dpi), dpi=dpi)
 
-ax2=fig.add_axes([0.7,0.7,0.3,0.3],frame_on=False)
-img2=img.copy()
-# img2[:,:,0]=img2[:,:,1]=img2[:,:,2]=.6*img[:,:,0]+.3*img[:,:,1]+.1*img[:,:,2]
-img2[:,:,0]=img2[:,:,1]=img2[:,:,2]=np.dot(img2,[.5,.3,.2])
-ax2.set_facecolor((1.0, 0.47, 0.42))
-ax2.imshow(img2, aspect='equal',extent = [0, 15, 0, 10])
+ax = fig.add_axes([0, 0, 1, 1])
+ax.tick_params(axis='both', bottom=False, left=False,
+               labelleft=False, labelbottom=False)
+# Read Image and Show it in Second Layer
+img = mpimg.imread(
+    f"{os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'resources', ))}/image.jpeg")
+ax1 = fig.add_axes([0.1, 0.1, .8, .8])
+ax1.tick_params(axis='both', bottom=False, left=False,
+                labelleft=False, labelbottom=False)
+imgplot = ax1.imshow(img, aspect=.5)
 
+# Convert Image to gray and Show it in Third Layer
+img2 = img.copy()
+img2[:, :, 0] = img2[:, :, 1] = img2[:, :, 2] = np.dot(img2, [.8, .4, 0.3])
+ax2 = fig.add_axes([0.6, 0.6, 0.3, 0.3])
+ax2.tick_params(axis='both', bottom=False, left=False,
+                labelleft=False, labelbottom=False)
+ax2.imshow(img2, cmap='gray', aspect=.5)
 
-ax1.tick_params(axis='both',bottom=False,left=False,labelleft=False, labelbottom=False)
-ax2.tick_params(axis='both',bottom=False,left=False,labelleft=False, labelbottom=False)
+# Prepairing for Sine Wave (set Forth Layer)
+ax3 = fig.add_axes([0.104, 0.3, .794, 0.05], frame_on=False)
+ax3.tick_params(axis='both', bottom=False, left=False,
+                labelleft=False, labelbottom=False)
+# creating a plot
+lines_plotted = ax3.plot([], color='black', linewidth=10)
 
+# putting limits on x, y since it is sin function
+plt.xlim(0, 2*np.pi)
+plt.ylim(-1.1, 1.1)
 
-from matplotlib.animation import FuncAnimation
-
-
-
-#________________________________
-y_new = np.sin(2 * np.pi * x)
-ax3=fig.add_axes([0,0.2,1,0.1],frame_on=False)
-ax3.tick_params(axis='both',bottom=False,left=False,labelleft=False, labelbottom=False)
-# Plot a sine wave using time and amplitude obtained for the sine wave
-
-# ax3.plot(x,y_new)
-# plt.show()
-
-#______________________
-# initializing a line variable
-line, = ax3.plot([], [], lw = 4)
-
-# data which the line will
-# contain (x, y)
-def init():
-	line.set_data([], [])
-	return line,
-
-def animate(i):
-	x = np.linspace(0, 2*np.pi, 500)
-
-	# plots a sine graph
-	y = np.sin(2 * np.pi * (x - 0.01 * i))
-	line.set_data(x, y)
-	
-	return line,
-
-anim = FuncAnimation(fig, animate, init_func = init,
-					frames = 200, interval = 20, blit = True)
+# initialising: x from 0 to 2∏
+x = np.linspace(0, 2*np.pi, 1000)
+y = 0
+line_plotted = lines_plotted[0]
+# function takes i as an input
 
 
-# plt.text(0,0,"DAEMON",bbox=dict(fill=False, edgecolor='red', linewidth=2),fontsize=38, size=50)
-
-fig.savefig('plot.jpeg',dpi=dpi)
-anim.save('continuousSineWave.gif', fps = 30)
-
+def AnimationFunction(i):
+    y = np.sin(10 * np.pi * (x - 0.01 * i))
+    line_plotted.set_data((x, y))
 
 
-# plt.show()
+# Used From: matplotlib.animation import FuncAnimation
+anim_created = FuncAnimation(fig, AnimationFunction, frames=100, interval=25)
+ax3.text(0, -9, "DAEMON", bbox=dict(fill=True, edgecolor='black',
+                                    facecolor="white", linewidth=2), fontweight="bold", fontsize=10, size=100)
 
+anim_created.save("../resources/anim.gif", fps=30)
+# fig.savefig('anim.jpeg')
+# For running in Colab uncomment this lines
+# video = anim_created.to_html5_video()
+# html = display.HTML(video)
+# display.display(html)
+plt.close()
